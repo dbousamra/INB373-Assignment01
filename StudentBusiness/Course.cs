331f2ee7e78cff599035466aa7362731b93c2ac5
Dominic Bou-Samra <domlebo70@Dominics-MacBook-Air.local>
2012-05-10 08:44:04 +1000

    Started on data binding layer

---
+++ StudentBusiness/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace StudentBusiness
+{
+    [DataObject(true)]
+    class Course
+    {
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public static List<Course> getAllCourses()
+        {
+
+        }
+
+
+    }
+}
---
+++ StudentObjects/Course.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace StudentObjects
+{
+    class Course
+    {
+
+        [DataObjectField(true)]
+        public long id {
+            get;
+            set;
+        }
+
+        [DataObjectField(false)]
+        public string name {
+            get;
+            set;
+        }
+
+        [DataObjectField(false)]
+        public long faculty_id {
+            get;
+            set;
+        }
+        [DataObjectField(false)]
+        public string course_code {
+            get;
+            set;
+        }
+    }
+}
